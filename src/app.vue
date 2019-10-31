943bb857ccd7435ed691befc43ea47b49eebd1e6
acid_martin@yahoo.com <martin.ivanov@catenamedia.com>
2019-10-31 15:58:59 +0100

    labels, donut mode, etc

--- src/app.vue
+++ src/app.vue
@@ -1,10 +1,9 @@
 <template>
   <div id="app">
-    <vc-piechart :series="series1" size="256px" />
-    <vc-piechart :series="series2" size="12em" />
+    <vc-piechart :series="series1" size="256px" :legend="true" title="Chart 1" :donut="true" />
+    <vc-piechart :series="series2" size="12em" :legend="true"  title="Chart 2" :donut="true" />
   </div>
 </template>
-
 <script>
 import VcPiechart from './components/vc-piechart/piechart'
 
@@ -17,26 +16,33 @@ export default {
     return {
       series1: [{
         color: '#f44336',
-        value: 40
+        value: 40,
+        label: 'Red'
       }, {
         color: '#ff9800',
-        value: 30
+        value: 30,
+        label: 'Orange'
       }, {
         color: '#4caf50',
-        value: 30
+        value: 30,
+        label: 'Green'
       }],
       series2: [{
         color: '#f44336',
-        value: 25
+        value: 25,
+        label: 'Red'
       }, {
         color: '#ff9800',
-        value: 25
+        value: 25,
+        label: 'Orange'
       }, {
         color: '#4caf50',
-        value: 25
+        value: 25,
+        label: 'Green'
       }, {
         color: '#9c27b0',
-        value: 25
+        value: 25,
+        label: 'Purple'
       }]
     }
   }
--- src/components/vc-piechart/piechart.vue
+++ src/components/vc-piechart/piechart.vue
@@ -1,15 +1,34 @@
 <template>
   <div :class="[name]" :id="id" v-if="series.length">
-    <div :class="[`${name}-container`]"></div>
+    <div :class="[`${name}-title`]" v-html="title" v-if="title" />
+    <div :class="[`${name}-container`]">
+      <div :class="[`${name}-donut`]" v-if="donut" />
+    </div>
+    <ul v-if="legend">
+      <li v-for="(item, index) in series" :key="index">
+        <span :style="{background: item.color}" /><span v-html="label(item)" />
+      </li>
+    </ul>
     <vc-style>
     #{{ id }},
-    #{{ id }} .{{ name }}-container {
+    #{{ id }} .{{ name }}-container
+    {
       width: {{ size }};
+    }
+
+    #{{ id }} .{{ name }}-container
+    {
       height: {{ size }};
     }
 
-    #{{ id }} .{{ name }}-container {
+    #{{ id }} .{{ name }}-container
+    {
       background: {{ segments }};
+    }
+
+    #{{ id }} .{{ name }}-donut
+    {
+      background: {{ parentBgColor }}
     }
     </vc-style>
   </div>
@@ -26,7 +45,8 @@ export default {
   data () {
     return {
       name: 'vc-piechart',
-      id: `vc-piechart-${uuid()}`
+      id: `vc-piechart-${uuid()}`,
+      parentBgColor: null
     }
   },
   computed: {
@@ -36,6 +56,23 @@ export default {
         segment => `${segment.color} 0 ${(gradient += segment.value)}%`
       )
       return `conic-gradient(${styles.join(',')})`
+    }
+  },
+  mounted () {
+    const noColor = 'rgba(0, 0, 0, 0)'
+    const documentColor = this.getBgColor(document.body)
+    let parentBgColor = this.getBgColor(this.$el.parentNode)
+    if (parentBgColor === noColor) {
+      parentBgColor = documentColor === noColor ? '#fff' : documentColor
+    }
+    this.parentBgColor = parentBgColor
+  },
+  methods: {
+    label (item) {
+      return `${item.label} (${item.value})`
+    },
+    getBgColor (node) {
+      return window.getComputedStyle(node, null).getPropertyValue('background-color')
     }
   },
   props: {
@@ -48,27 +85,102 @@ export default {
       type: Array,
       required: false,
       default: () => []
+    },
+    legend: {
+      type: Boolean,
+      required: false,
+      default: true
+    },
+    title: {
+      type: String,
+      required: false
+    },
+    donut: {
+      type: Boolean,
+      required: false
     }
   }
 }
 </script>
 <style>
 .vc-piechart,
-.vc-piechart-container
+.vc-piechart-container,
+.vc-piechart span:first-child,
+.vc-piechart .vc-piechart-container .vc-piechart-donut
 {
   border-radius: 100%;
+}
+
+.vc-piechart,
+.vc-piechart *
+{
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+.vc-piechart li,
+.vc-piechart span
+{
+  display: inline-block;
+  vertical-align: middle;
+}
+
+.vc-piechart ul,
+.vc-piechart .vc-piechart-title
+{
+  text-align: center;
+  font-size: .8em;
+}
+
+.vc-piechart .vc-piechart-container
+{
+  box-shadow: 0 2px 4px -1px rgba(0, 0, 0, .2),
+    0 4px 5px 0 rgba(0, 0, 0, .14),
+    0 1px 10px 0 rgba(0, 0, 0, .12);
+  position: relative;
+}
+
+.vc-piechart .vc-piechart-container .vc-piechart-donut
+{
+  width: 40%;
+  height: 40%;
+  position: absolute;
+  background: #fff;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  box-shadow: 0 2px 4px -1px rgba(0, 0, 0, .2) inset,
+    0 4px 5px 0 rgba(0, 0, 0, .14) inset,
+    0 1px 10px 0 rgba(0, 0, 0, .12) inset;
 }
 
 .vc-piechart
 {
   user-select: none;
   display: block;
+  font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
+  color: #303030;
 }
 
-.vc-piechart
+.vc-piechart .vc-piechart-title
 {
-  box-shadow: 0 2px 4px -1px rgba(0, 0, 0, .2),
-    0 4px 5px 0 rgba(0, 0, 0, .14),
-    0 1px 10px 0 rgba(0, 0, 0, .12);
+  font-weight: 500;
+  font-size: 1em;
+  line-height: 1.5em;
+}
+
+.vc-piechart li
+{
+  padding: .2em;
+}
+
+.vc-piechart span:first-child
+{
+  width: .85em;
+  height: .85em;
+  margin: 0 .2em 0 0;
 }
 </style>
